65ff37eb9cbcdec2e9cbf2cea865e97558d257ec
Исламбек Жакыпбеков <izhakypbekov@teksher.local>
2026-07-30 10:52:04 +0600

    End

--- src/main/java/kg/teksher/api/repository/ScanRepository.java
+++ src/main/java/kg/teksher/api/repository/ScanRepository.java
@@ -3,5 +3,9 @@ package kg.teksher.api.repository;
 import kg.teksher.api.entity.Scan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScanRepository extends JpaRepository<Scan, Long> {
+    List<Scan> findByUserId(String userId);
 }
+
